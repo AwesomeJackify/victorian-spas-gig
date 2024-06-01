6ac7cbb107c9cdbb81d86514cddf546b89a4d9c7
Jack Gong <jackgong48@gmail.com>
2024-06-01 12:57:19 +1200

    PROPERLY: added partytown to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()]
+  integrations: [tailwind(), react(), icon(), partytown()]
 });
